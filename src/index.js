88a3ce8e40ddc52f8aed80c8babde4c2734081eb
Miguel601 <henriquez.miguel@outlook.com>
2019-05-31 11:15:56 -0400

    NAILED IIIT

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-const listContainer = document.querySelector('.list-group')
-const singleBeerContainer = document.querySelector('#beer-detail')
+    const listContainer = document.querySelector('.list-group')
+    const singleBeerContainer = document.querySelector('#beer-detail')
 
 
-function getAllBeers(){
-    fetch('http://localhost:3000/beers')
-    .then(response => response.json())
-    .then(beers => beers.forEach(slapBeersOnList))
-}
+    function getAllBeers() {
+        fetch('http://localhost:3000/beers')
+            .then(response => response.json())
+            .then(beers => beers.forEach(slapBeersOnList))
+    }
 
 
-function slapBeersOnList(beer){
-    listContainer.innerHTML += `
+    function slapBeersOnList(beer) {
+        listContainer.innerHTML += `
     <li class="list-group-item" data-id="${beer.id}" >${beer.name}</li>
     `
-}
+    }
 
 
-listContainer.addEventListener('click', getAndShowBeer)
+    listContainer.addEventListener('click', getAndShowBeer)
 
 
-function getAndShowBeer(event){
-    const beerId = event.target.dataset.id
+    function getAndShowBeer(event) {
+        const beerId = event.target.dataset.id
 
-    fetch(`http://localhost:3000/beers/${beerId}`)
-    .then(response => response.json())
-    .then(beer => singleBeerDetails(beer))
-}
+        fetch(`http://localhost:3000/beers/${beerId}`)
+            .then(response => response.json())
+            .then(beer => singleBeerDetails(beer))
+    }
 
 
-function singleBeerDetails(beer){
-    singleBeerContainer.innerHTML = `
+    function singleBeerDetails(beer) {
+        singleBeerContainer.innerHTML = `
 <h1>${beer.name}</h1>
 <img src="${beer.image_url}">
 <h3>${beer.tagline}</h3>
@@ -39,37 +39,39 @@ function singleBeerDetails(beer){
   Save
 </button>
     `
-}
+    }
 
-singleBeerContainer.addEventListener('click', editBeer)
+    singleBeerContainer.addEventListener('click', editBeer)
 
 
-function editBeer(event){
-    const description = document.getElementById('text').value
-    const beerId = event.target.dataset.id
-
-    fetch(`http://localhost:3000/beers/${beerId}`, {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body:JSON.stringify({
-            description: description
-        })
-    })
-    .then(response => response.json())
-    .then(beer => {
-            listContainer.innerHTML += `
+    function editBeer(event) {
+        event.preventDefault()
+        const beerId = event.target.dataset.id
+        const description = document.getElementById('text').value
+        
+        if(event.target.innerText === "Save" ){
+        fetch(`http://localhost:3000/beers/${beerId}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify({
+                    description: description
+                })
+            })
+            .then(response => response.json())
+            .then(beer => {
+                listContainer.innerHTML + `
             <li class="list-group-item" data-id="${beer.id}" >${beer.name}</li>
             `
-    })
-}
+            })}
+    }
 
 
 
 
-getAllBeers()
+    getAllBeers()
 
 
 })
