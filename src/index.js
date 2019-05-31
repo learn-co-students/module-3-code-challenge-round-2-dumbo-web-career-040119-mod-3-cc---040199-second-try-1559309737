60221a8828322a69c6f0ce72e7ad59d99328d922
github username <github email address>
2019-05-31 10:57:37 -0400

    about to work on opimistic

---
+++ src/index.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const beerUl = document.querySelector('#list-group')
+
+
+  fetch(`http://localhost:3000/beers`)
+  .then(response => response.json())
+  .then(beers => beers.forEach(listOfBeers))
+
+
+  function listOfBeers(beer){
+    const beerLi = document.createElement('li')
+    beerLi.className = 'list-group-item'
+    beerLi.innerHTML = beer.name
+    beerUl.append(beerLi)
+    beerLi.addEventListener('click', (event)=> {
+      beerShow(beer, event)
+    })
+  }
+
+  function beerShow(beer,event){
+    const beerDiv = document.querySelector('#beer-detail')
+    beerDiv.innerHTML = `<h1>${beer.name}</h1>
+    <img src="${beer.image_url}">
+    <h3>${beer.tagline}</h3>
+    <textarea id="beer-text">${beer.description}</textarea>
+    <button data-id=${beer.id} id="edit-beer" class="btn btn-info">
+      Save
+    </button>`
+  }
+  const beerDiv = document.querySelector('#beer-detail')
+  const saveBtn = document.querySelector('#edit-beer')
+
+  beerDiv.addEventListener('click', (event) => {
+    if(event.target.className == 'btn btn-info'){
+      saveBeer(event.target.parentNode.parentNode,event.target.dataset.id)
+    }
+  })
+
+  function saveBeer(div,id){
+    const beerText = div.querySelector('#beer-text').value
+    fetch(`http://localhost:3000/beers/${id}`, {
+      method:"PATCH",
+      headers: {
+        'Content-Type':'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({description: beerText})
+    })
+    .then(response => response.json())
+    .then(updated => beerShow(updated))
+  }
+
+
+})
